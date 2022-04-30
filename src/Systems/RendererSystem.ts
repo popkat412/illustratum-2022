9e745a9690b6dc22447e518c4b53a42599629994
wangy412 <wangyunze412@gmail.com>
2022-04-30 10:57:54 +0800

    refactor: update pixi graphics in RendererSystem

--- src/Systems/RendererSystem.ts
+++ src/Systems/RendererSystem.ts
@@ -1,20 +1,59 @@
+import ParticleComponent from "../Components/ParticleComponent";
 import PixiGraphicsRenderComponent from "../Components/PIXIGraphicsRenderComponent";
 import System from "../EntitySystem/System";
-import { HasPixiApp } from "../Environments/EnvironmentInterfaces";
+import {
+  HasPixiApp,
+  HasRenderScale,
+} from "../Environments/EnvironmentInterfaces";
 
-export default class RendererSystem<E extends HasPixiApp> extends System<E> {
+export default class RendererSystem<
+  E extends HasPixiApp & HasRenderScale
+> extends System<E> {
   setup() {
     for (const [
-      _,
-      pixiGraphicsRenderComponent,
+      entity,
+      pixiGraphicsComponent,
     ] of this.entityManager.allEntitiesWithComponent<PixiGraphicsRenderComponent>(
       PixiGraphicsRenderComponent
     )) {
-      this.environment.app.stage.addChild(
-        pixiGraphicsRenderComponent.pixiGraphics
-      );
+      this.environment.app.stage.addChild(pixiGraphicsComponent.pixiGraphics);
+
+      const particleComponent =
+        this.entityManager.getComponent<ParticleComponent>(
+          entity,
+          ParticleComponent
+        );
+      if (particleComponent) {
+        this.updateGraphicsComponent(pixiGraphicsComponent, particleComponent);
+      }
     }
   }
 
-  update() {}
+  update() {
+    for (const [
+      entity,
+      pixiGraphicsComponent,
+    ] of this.entityManager.allEntitiesWithComponent<PixiGraphicsRenderComponent>(
+      PixiGraphicsRenderComponent
+    )) {
+      const particleComponent =
+        this.entityManager.getComponent<ParticleComponent>(
+          entity,
+          ParticleComponent
+        );
+      if (particleComponent) {
+        this.updateGraphicsComponent(pixiGraphicsComponent, particleComponent);
+      }
+    }
+  }
+
+  private updateGraphicsComponent(
+    pixiGraphicsComponent: PixiGraphicsRenderComponent,
+    particleComponent: ParticleComponent
+  ) {
+    pixiGraphicsComponent.pixiGraphics.x =
+      particleComponent.pos.x * this.environment.scaleFactor;
+    pixiGraphicsComponent.pixiGraphics.y =
+      particleComponent.pos.y * this.environment.scaleFactor;
+  }
 }
